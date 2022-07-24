f457a33b9e04c5df3c8eecff27362b52464efaa6
PhysBoom <mc_gold05@yahoo.com>
2022-07-24 10:02:52 -0700

    Final V1 build

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 from crypto_blueprint import crypto_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build/static', template_folder='build')
 
 app.register_blueprint(crypto_blueprint)
+
+# Default route to serve the React build file from build/index.html.
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET'])
+def serve(path):
+    return render_template('index.html')
 
 if __name__ == "__main__":
     load_dotenv()
